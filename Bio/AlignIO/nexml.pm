8bfeb220c6089c04c8ccf860cd2ceea83beffdca
chmille4 <chmille4@localhost>
2009-06-01 22:00:30 +0000

    Adding preliminary version of of Bio::SeqIO::nexml->next_seq() and Bio::AlignIO::next_aln() methods.  More mapping of attributes could probably be done and the error handling needs to be more robust.
    svn path=/bioperl-dev/trunk/; revision=15735

---
+++ Bio/AlignIO/nexml.pm
@@ -0,0 +1,179 @@
+# $Id:
+#
+# BioPerl module for Bio::AlignIO::nexml
+#
+# Copyright Chase Miller
+#
+# You may distribute this module under the same terms as perl itself
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::AlignIO::nexml - nexml sequence input/output stream
+
+=head1 SYNOPSIS
+
+Do not use this module directly.  Use it via the L<Bio::AlignIO> 
+class.
+
+=head1 DESCRIPTION
+
+This object can transform L<Bio::SimpleAlign> objects to and from
+nexml files.
+
+=head1 FEEDBACK
+
+=head2 Support 
+ 
+Please direct usage questions or support issues to the mailing list:
+  
+L<bioperl-l@bioperl.org>
+  
+rather than to the module maintainer directly. Many experienced and 
+reponsive experts will be able look at the problem and quickly 
+address it. Please include a thorough description of the problem 
+with code and data examples if at all possible.
+
+=head2 Reporting Bugs
+
+Report bugs to the Bioperl bug tracking system to help us keep track
+the bugs and their resolution.  Bug reports can be submitted via the
+web:
+
+  http://bugzilla.open-bio.org/
+
+=head1 AUTHORS
+
+Chase Miller
+
+=head1 CONTRIBUTORS
+
+Mark Jenson, maj@fortinbras.us
+Rutger Vos, rutgeraldo@gmail.com
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object
+methods. Internal methods are usually preceded with a _
+
+=cut
+
+# Let the code begin...
+
+package Bio::AlignIO::nexml;
+
+use strict;
+use Bio::Phylo::IO qw(parse unparse);
+use Bio::LocatableSeq;
+use base qw(Bio::AlignIO);
+
+
+=head2 next_aln
+
+ Title   : next_aln
+ Usage   : $aln = $stream->next_aln
+ Function: returns the next alignment in the stream.
+ Returns : Bio::Align::AlignI object - returns 0 on end of file
+	        or on error
+ Args    : 
+
+See L<Bio::Align::AlignI>
+
+=cut
+
+sub next_aln {
+	my ($self) = @_;
+    unless ( $self->{'_parsed'} ) {
+    	#use a parse function to load all the alignment objects found in the nexml file at once
+        $self->_parse;
+    }
+    return $self->{'_alns'}->[ $self->{'_alnsiter'}++ ];
+}
+
+sub _parse {
+	my ($self) = @_;
+
+    $self->{'_parsed'}   = 1;
+    $self->{'_alnsiter'} = 0;
+	
+	
+	#
+	my $proj = parse(
+ 	'-file'       => $self->{'_file'},
+ 	'-format'     => 'nexml',
+ 	'-as_project' => '1'
+ 	);
+
+	my ($start, $end, $name, $seqname, $seq, $seqchar, $entry, 
+		 $tempname, $tempdesc, %align, $desc, $maxlen);
+	my $aln = Bio::SimpleAlign->new();
+
+	while (my $data_obj = $proj->next()) {
+	    
+	    #check if Bio::Phylo section(not sure if thats the right name) is of the right class
+ 		if (ref $data_obj ne 'Bio::Phylo::Matrices::Matrix')
+ 		{
+ 			next;
+ 		}
+ 		#check if mol_type is something that makes sense to be a seq
+ 		my $mol_type = lc($data_obj->get_type());
+ 		unless ($mol_type eq 'dna' || $mol_type eq 'rna' || $mol_type eq 'protein')
+ 		{
+ 			next;
+ 		}
+ 		
+ 		my $rows = $data_obj->get_entities();
+ 		my $basename = $data_obj->get_name();
+ 		my $seqNum = 0;
+ 		foreach my $row (@$rows)
+ 		{
+ 			my $newSeq = $row->get_char();
+ 			
+ 			$seqNum++;
+ 			my $seqID = "$basename.row_$seqNum";
+ 	
+ 			$seq = Bio::LocatableSeq->new(
+						  -seq         => $newSeq,
+						  -display_id  => "$seqID",
+						  -description => $desc,
+						  -start       => $start,
+						  -end         => $end,
+						  -alphabet	   => $mol_type,
+						  );
+			#what other data is appropriate to pull over from bio::phylo::matrices::matrix??
+		    $aln->add_seq($seq);
+		    $self->debug("Reading r$seqID\n");
+ 		
+ 			
+ 		}
+ 		push @{ $self->{'_alns'} }, $aln;
+ 	}
+ 	if(@{ $self->{'_alns'} } == 0)
+ 	{
+ 		self->debug("no seqs in $self->{_file}");
+ 	}
+}
+
+=head2 write_aln
+
+ Title   : write_aln
+ Usage   : $stream->write_aln(@aln)
+ Function: writes the $aln object into the stream in nexml format
+ Returns : 1 for success and 0 for error
+ Args    : L<Bio::Align::AlignI> object
+
+See L<Bio::Align::AlignI>
+
+=cut
+
+sub write_aln {
+    # not implemented yet
+}
+
+
+
+
+
+
+
+1;
---
+++ Bio/SeqIO/nexml.pm
@@ -0,0 +1,189 @@
+# $Id: fasta.pm 15549 2009-02-21 00:48:48Z maj $
+# BioPerl module for Bio::SeqIO::nexml
+#
+# Please direct questions and support issues to <bioperl-l@bioperl.org> 
+#
+# Cared for by Chase Miller <chmille4@gmail.com>
+#
+# Copyright Chase Miller
+#
+# You may distribute this module under the same terms as perl itself
+# _history
+# May, 2009  Largely written by Chase Miller
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::SeqIO::nexml - nexml sequence input/output stream
+
+=head1 SYNOPSIS
+
+Do not use this module directly.  Use it via the Bio::SeqIO class.
+
+=head1 DESCRIPTION
+
+This object can transform Bio::Seq objects to and from nexml xml files.
+
+=head1 FEEDBACK
+
+=head2 Mailing Lists
+
+User feedback is an integral part of the evolution of this and other
+Bioperl modules. Send your comments and suggestions preferably to one
+of the Bioperl mailing lists.  Your participation is much appreciated.
+
+  bioperl-l@bioperl.org                  - General discussion
+  http://bioperl.org/wiki/Mailing_lists  - About the mailing lists
+
+=head2 Support 
+ 
+Please direct usage questions or support issues to the mailing list:
+  
+L<bioperl-l@bioperl.org>
+  
+rather than to the module maintainer directly. Many experienced and 
+reponsive experts will be able look at the problem and quickly 
+address it. Please include a thorough description of the problem 
+with code and data examples if at all possible.
+
+=head2 Reporting Bugs
+
+Report bugs to the Bioperl bug tracking system to help us keep track
+the bugs and their resolution.  Bug reports can be submitted via the
+web:
+
+  http://bugzilla.open-bio.org/
+
+=head1 AUTHORS - Chase Miller
+
+Email: chmille4@gmail.com
+
+=head1 CONTRIBUTORS
+
+Mark Jenson, maj@fortinbras.us
+Rutger Vos, rutgeraldo@gmail.com
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object
+methods. Internal methods are usually preceded with a _
+
+=cut
+
+# Let the code begin...
+
+package Bio::SeqIO::nexml;
+
+use strict;
+
+use Bio::Seq::SeqFactory;
+use Bio::Phylo::IO qw (parse unparse);
+use base qw(Bio::SeqIO);
+
+sub _initialize {
+  my($self,@args) = @_;
+  $self->SUPER::_initialize(@args);  
+}
+
+=head2 next_seq
+
+ Title   : next_seq
+ Usage   : $seq = $stream->next_seq()
+ Function: returns the next sequence in the stream
+ Returns : Bio::Seq object
+ Args    : NONE
+
+=cut
+
+sub next_seq {
+	my ($self) = @_;
+    unless ( $self->{'_parsed'} ) {
+    	#use a parse function to load all the sequence objects found in the nexml file at once
+        $self->_parse;
+    }
+    return $self->{'_seqs'}->[ $self->{'_seqiter'}++ ];
+}
+
+sub _parse {
+	my ($self) = @_;
+
+    $self->{'_parsed'}   = 1;
+    $self->{'_seqiter'} = 0;
+	
+	
+	#
+	my $proj = parse(
+ 	'-file'       => $self->{'_file'},
+ 	'-format'     => 'nexml',
+ 	'-as_project' => '1'
+ 	);
+ 
+ 	while (my $data_obj = $proj->next())
+ 	{
+ 		#check if Bio::Phylo section(not sure if thats the right name) is of the right class
+ 		if (ref $data_obj ne 'Bio::Phylo::Matrices::Matrix')
+ 		{
+ 			next;
+ 		}
+ 		#check if mol_type is something that makes sense to be a seq
+ 		my $mol_type = lc($data_obj->get_type());
+ 		unless ($mol_type eq 'dna' || $mol_type eq 'rna' || $mol_type eq 'protein')
+ 		{
+ 			next;
+ 		}
+ 		
+ 		my $rows = $data_obj->get_entities();
+ 		my $seqnum = 0;
+ 		my $basename = $data_obj->get_name();
+ 		foreach my $row (@$rows)
+ 		{
+ 			my $newSeq = $row->get_char();
+ 			$seqnum++;
+ 			#generate full sequence id
+ 			my $seqID = "$basename.seq_$seqnum";
+ 			
+ 			my $seq = Bio::PrimarySeq->new();
+ 			$seq->seq($newSeq);
+ 			$seq->alphabet($mol_type);
+ 			$seq->id("$seqID");
+ 			#what other data is appropriate to pull over from bio::phylo::matrices::matrix??
+ 		
+ 			push @{ $self->{'_seqs'} }, $seq;
+ 		}
+ 	}
+ 	unless(@{ $self->{'_seqs'} } == 0)
+ 	{
+# 		self->debug("no seqs in $self->{_file}");
+ 	}
+ }
+ 
+ 
+ 
+
+=head2 write_seq
+
+ Title   : write_seq
+ Usage   : $stream->write_seq(@seq)
+ Function: Writes the $seq object into the stream
+ Returns : 1 for success and 0 for error
+ Args    : Array of 1 or more Bio::PrimarySeqI objects
+
+=cut
+
+sub write_seq { #in progress
+ #  my ($self,@seq) = @_;
+ #  foreach my $seq (@seq) {
+ #  		my $dat = Bio::Phylo::Matrices::Datum->new_from_bioperl($seq);
+ #  		$self->_print( $dat->to_xml );
+ #  }
+ #  $self->flush if $self->_flush_on_write && defined $self->_fh;
+ #  return 1;
+}
+
+
+
+
+
+
+1;
