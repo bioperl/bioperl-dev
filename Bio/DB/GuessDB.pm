1941f85fa84cc073ccd46992445971f98d5abebb
chmille4 <chmille4@localhost>
2009-11-07 19:13:19 +0000

    Added Han's code for determining NCBI, and UNIPROT accesions
    svn path=/bioperl-dev/branches/guess-db/; revision=16337

--- Bio/DB/GuessDB.pm
+++ Bio/DB/GuessDB.pm
@@ -2,9 +2,9 @@
 #
 # Please direct questions and support issues to <bioperl-l@bioperl.org> 
 #
-# Cared for by Chase Miller <chmille4 at gmail dot com>
+# Cared for by Hans-Rudolf Hotz <hans-rudolf.hotz@fmi.ch>
 #
-# Copyright Chase Miller, Hans-Rudolf Hotz
+# Copyright Hans-Rudolf Hotz, Chase Miller
 #
 # You may distribute this module under the same terms as perl itself
 #
@@ -56,13 +56,13 @@ Bug reports can be submitted via the web.
 
   http://bugzilla.open-bio.org/
 
-=head1 AUTHOR
-
-Chase Miller
-Email chmille4 at gmail dot com
+=head1 AUTHORS
 
 Hans-Rudolf Hotz
 hans-rudolf.hotz@fmi.ch
+
+Chase Miller
+Email chmille4 at gmail dot com
 
 
 =head1 APPENDIX
@@ -80,7 +80,93 @@ package Bio::DB::GuessDB;
 use strict;
 
 sub guessDB {
-	my ($self, $accession) = @_;
+	my ($accession) = @_;
+
+	my %database;
+	$database{'id_type'} = "accession";
+	if ($accession =~ /^(.+)\.\d+$/) {
+		$database{'id_type'} = "sequence_version";
+		$accession = $1;
+	}	
 	
-	#add logic
+	# test for RefSeq based on http://www.ncbi.nlm.nih.gov/RefSeq/key.html#accession
+	if ($accession =~ /^[ANXY]P_\d{6}$/ || $accession =~ /^ZP_\d{8}$/ || $accession =~ /^[NXY]P_\d{9}$/ ) {
+		$database{'name'} = "RefSeq";
+		$database{'type'} = "protein";
+	}
+	elsif ($accession =~ /^AC_\d{6}$/ || $accession =~ /^N[CGMRTWS]_\d{6}$/ || $accession =~ /^X[MR]_\d{6}$/ ) {
+		$database{'name'} = "RefSeq";
+		$database{'type'} = "nucleotide";
+	}
+	elsif ($accession =~ /^N[MW]_\d{9}$/ || $accession =~ /^NZ_\w{4}\d{8}$/ ||$accession =~ /^XM_\d{9}$/ ) {
+		$database{'name'} = "RefSeq";
+		$database{'type'} = "nucleotide";
+	}		
+
+	# test for UniProtKB based on http://www.uniprot.org/manual/accession_numbers
+	elsif ($accession =~ /^[A-N][0-9][A-Z][A-Z0-9][A-Z0-9][0-9]$/ ) {
+		$database{'name'} = "UniProtKB";
+		$database{'type'} = "protein";
+	}
+	elsif ($accession =~ /^[R-Z][0-9][A-Z][A-Z0-9][A-Z0-9][0-9]$/ ) {
+		$database{'name'} = "UniProtKB";
+		$database{'type'} = "protein";
+	}
+	elsif ($accession =~ /^[OPQ][0-9][A-Z0-9][A-Z0-9][A-Z0-9][0-9]$/ ) {
+		$database{'name'} = "UniProtKB";
+		$database{'type'} = "protein";
+	}
+	# test for UniProtKB entry name based on http://www.uniprot.org/manual/entry_name
+	elsif ($accession =~ /^[A-Z0-9]{1,5}_[A-Z0-9]{1,5}$/ ) {
+		$database{'name'} = "UniProtKB";
+		$database{'type'} = "protein";
+		$database{'id_type'} = "entry_name";
+	}
+	
+	# test for Genbank accession format based on the release notes (section  3.4.6): ftp://ftp.ncbi.nih.gov/genbank/gbrel.txt
+	elsif ($accession =~ /^[A-Z]\d{5}$/ || $accession =~ /^[A-Z][A-Z]\d{6}$/ ) {
+		$database{'name'} = "GenBank";
+		$database{'type'} = "nucleotide";
+	}	
+		
+	# test for ensembl  (waiting for confirmation from ensembl, also a real validation of those organism triplets would be nice)
+	elsif ($accession =~ /^ENS([A-Z]{3})?G\d{11}$/ ) {
+		$database{'name'} = "Ensembl";
+		$database{'type'} = "gene";
+	}
+	elsif ($accession =~ /^ENS([A-Z]{3})?T\d{11}$/ ) {
+		$database{'name'} = "Ensembl";
+		$database{'type'} = "nucleotide";
+	}
+	elsif ($accession =~ /^ENS([A-Z]{3})?P\d{11}$/ ) {
+		$database{'name'} = "Ensembl";
+		$database{'type'} = "protein";
+	}
+	
+	# a functional test for Entrez Gene
+	elsif ($accession =~ /^\d+$/ ) {
+		my $foo = get("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=gene&id=$accession&retmode=xml");
+		if ($foo =~ /Empty id list/ ) {
+			$database{'name'} = "notrecognized";
+			$database{'type'} = "unknown";
+			$database{'id_type'} = "unknown";
+		}
+		else {
+			$database{'name'} = "Gene";
+			$database{'type'} = "gene";
+		}	
+		# just to be nice with the NCBI server
+		sleep(2)
+		# call to NCBI should be replaced wit a call to our local Gene database, once we have it.....
+	}
+	
+	
+	else {
+		$database{'name'} = "notrecognized";
+		$database{'type'} = "unknown";
+		$database{'id_type'} = "unknown";
+	}	
+		
+	return \%database;
 }
+
